d655b92494f865d58fa4f85ac1f15cdc3cdf7cde
Pierce Alworth <pbalworth@gmail.com>
2020-03-30 11:53:26 -0500

    Final commit.

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,13 +1,17 @@
 class FoodController < ApplicationController
     def index
       food = params[:q]
-      food_collection = food.gsub(" ","20%")        
+      food_collection = food.split(/ /)
+      food_word_1 = food_collection[0]
+      food_word_2 = food_collection[1]
+    #   food_collection = food.gsub(" ", "20%")
+        
       conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
          faraday.headers["api_key"] = ENV["FOOD_DATA_API_KEY"]
       end
 
         
-      response = conn.get("/fdc/v1/search?api_key=#{ENV["FOOD_DATA_API_KEY"]}\&generalSearchInput=#{food_collection}")
+      response = conn.get("/fdc/v1/search?api_key=#{ENV["FOOD_DATA_API_KEY"]}\&generalSearchInput=#{food_word_1}%20#{food_word_2}")
 
       json = JSON.parse(response.body, symbolize_names: true)
       @foods = json[:foods]
