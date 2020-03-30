a611c5f423e77ef2f0da6a74e9996c19ea27b5a7
Pierce Alworth <pbalworth@gmail.com>
2020-03-30 11:21:44 -0500

    Update spec so it is accurate, test passing

--- spec/user_can_search_by_food_spec.rb
+++ spec/user_can_search_by_food_spec.rb
@@ -21,17 +21,18 @@ feature "User can search for foods " do
         
     click_on "Search"
     expect(current_path).to eq("/foods")
+      save_and_open_page
     # Then my path should be "/search" with "state=CO" in the parameters
-    expect(page).to have_content("7 Results")
+    expect(page).to have_content("50 Results")
     # And I should see a message "7 Results"
-    expect(page).to have_css(".member", count: 7)
+    expect(page).to have_css(".food", count: 50)
     # And I should see a list of 7 the members of the house for Colorado
 
-    within(first(".member")) do
-      expect(page).to have_css(".name")
-      expect(page).to have_css(".role")
-      expect(page).to have_css(".party")
-      expect(page).to have_css(".district")
+    within(first(".food")) do
+      expect(page).to have_css(".brandOwner")
+      expect(page).to have_css(".gtinUpc")
+      expect(page).to have_css(".description")
+      expect(page).to have_css(".ingredients")
     end
     # And they should be ordered by seniority from most to least
     # And I should see a name, role, party, and district for each member
