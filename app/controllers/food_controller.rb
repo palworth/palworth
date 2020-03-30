8dbefd501dcd67033cd242fbbcb98e6bdd817a22
Pierce Alworth <pbalworth@gmail.com>
2020-03-30 11:25:02 -0500

    Add response to controller

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,19 +1,18 @@
 class FoodController < ApplicationController
     def index
-        food = params[:q]
-        food_collection = food.split(/ /)
-        food_word_1 = food_collection[0]
-        food_word_2 = food_collection[1]
+      food = params[:q]
+      food_collection = food.split(/ /)
+      food_word_1 = food_collection[0]
+      food_word_2 = food_collection[1]
         
-        conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
-            faraday.headers["api_key"] = 'xvfkfIcQZo29ZuBs2BFPSkibeYZGKcaUeg6pZYk2'
-        end
-        # binding.pry
-        
-        response = conn.get("/fdc/v1/search?api_key=xvfkfIcQZo29ZuBs2BFPSkibeYZGKcaUeg6pZYk2\&generalSearchInput=#{food_word_1}%20#{food_word_2}")
-        binding.pry
+      conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
+         faraday.headers["api_key"] = 'xvfkfIcQZo29ZuBs2BFPSkibeYZGKcaUeg6pZYk2'
+      end
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    @foods = json[:results]
+        
+      response = conn.get("/fdc/v1/search?api_key=xvfkfIcQZo29ZuBs2BFPSkibeYZGKcaUeg6pZYk2\&generalSearchInput=#{food_word_1}%20#{food_word_2}")
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      @foods = json[:foods]
     end
 end
