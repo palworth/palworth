9672e13958fe3eb3a2b87f89f1a665b9a6372aec
Pierce Alworth <pbalworth@gmail.com>
2020-03-30 11:05:18 -0500

    Just got access to the response I need.

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,13 +1,17 @@
 class FoodController < ApplicationController
     def index
         food = params[:q]
+        food_collection = food.split(/ /)
+        food_word_1 = food_collection[0]
+        food_word_2 = food_collection[1]
         
         conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
             faraday.headers["api_key"] = 'xvfkfIcQZo29ZuBs2BFPSkibeYZGKcaUeg6pZYk2'
-            binding.pry
-    end
-
-    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+        end
+        # binding.pry
+        
+        response = conn.get("/fdc/v1/search?api_key=xvfkfIcQZo29ZuBs2BFPSkibeYZGKcaUeg6pZYk2\&generalSearchInput=#{food_word_1}%20#{food_word_2}")
+        binding.pry
 
     json = JSON.parse(response.body, symbolize_names: true)
     @foods = json[:results]
